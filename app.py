97abc6832910c4d81d1a28f5827ab92fbace3d8e
activezonepol <info@activezone.pl>
2026-04-25 20:01:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -673,7 +673,7 @@ with st.sidebar:
         key="nav_top_radio",
         label_visibility="collapsed",
         on_change=lambda: (st.session_state.update({'last_page': st.session_state['nav_top_radio']}), save_to_supabase())
-
+    )
     # --- SEKCJA ATRAKCJI wbudowana w nawigację ---
     # Przycisk ＋ DODAJ ATRAKCJE/MIEJSCE
     st.markdown(
