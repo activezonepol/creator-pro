a5bb41651dcdc534cfafd2c6111158d2df6ebd9b
activezonepol <info@activezone.pl>
2026-04-27 00:18:11 +0200

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -1740,6 +1740,7 @@ def build_presentation(current_page="Strona Tytułowa", export_mode=False):
         </div>{fh}""", "slide-app"))
 
     # --- Branding ---
+        if not get_data('brand_hide', False):
         b1 = get_b64('img_brand_1', (1, 1))
         b2 = get_b64('img_brand_2', (1, 1))
         b3 = get_b64('img_brand_3', (16, 9))
@@ -1753,7 +1754,7 @@ def build_presentation(current_page="Strona Tytułowa", export_mode=False):
                 <div class="info-col" style="flex: 55; padding-right: 30px; padding-top: 24px; justify-content: flex-start;">
                     <div class="app-overline-style"><span>{str(get_data('brand_overline',''))}</span></div>
                     <div class="title-h1" style="margin-bottom: 10px; font-size:{fs_h1_val-8}px;">{str(get_data('brand_title','')).replace(chr(10),'<br>')}</div>
-                    <div class="title-sub" style="margin-bottom:14px; font-size:{max(10,fs_sub_6)}px;">{str(get_data('brand_subtitle','')).replace(chr(10),'<br>')}</div>
+                    <div class="title-sub" style="margin-bottom:14px; font-size:{max(10, fs_sub_val-6)}px;">{str(get_data('brand_subtitle','')).replace(chr(10),'<br>')}</div>
                     <ul class="app-list" style="margin-top:0;">{bfh}</ul>
                 </div>
                 <div style="flex: 50; position: relative; height: 100%;">
