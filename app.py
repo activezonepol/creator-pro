9b8de67b07fd72a456dbf762d93247f97c6c4cb5
activezonepol <info@activezone.pl>
2026-04-29 15:07:30 +0200

    Import _build_proj_dict from data_utils

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import time
 import streamlit as st
 from db_utils import save_to_supabase, fetch_all_offers
 from supabase import create_client, Client
+from data_utils import _build_proj_dict
 from my_components import safe_text_input, safe_text_area
 from renderer import (
     COUNTRIES_DICT, FONTS_LIST, hotel_icons, icon_map, defaults, IMAGE_KEYS,
