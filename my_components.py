d20b355667a1b4c99939d5e24e7ac3cbcff2d3af
activezonepol <info@activezone.pl>
2026-05-03 22:24:31 +0200

    Fix: safe_text_input/area now properly forwards args/kwargs to user_on_change (systemic)
    Improved text input and text area functions to support on_change callbacks with arguments. Ensured value synchronization even after multiple widget remounts.

--- my_components.py
+++ my_components.py
@@ -5,6 +5,9 @@ Custom widgety Streamlita z buforowaniem (zapobiegają reload przy każdym znaku
 
 ROZWIAZANIE BUG'a: Po wielokrotnym przelaczeniu slajdow widget tracil wartosc.
 Naprawa: Wymuszamy `value=` w text_input zamiast polegac tylko na `key=`.
+
+Python 3.14 / Streamlit nowsza wersja: Przekazujemy args/kwargs do user_on_change
+zeby callbacki typu set_focus(target_id) mogly otrzymac argumenty.
 """
 import streamlit as st
 
@@ -14,8 +17,7 @@ def safe_text_input(label, key, **kwargs):
     Text input z buforem - nie przeladowuje na kazdy znak.
     Synchronizuje buffer z glownym kluczem przy kazdym wywolaniu.
     
-    NAPRAWA: Wymuszamy `value=` zeby Streamlit nie tracil wartosci 
-    po wielokrotnym odmontowaniu/zmontowaniu widgetu.
+    Wspiera on_change z args= i kwargs= (jak natywny st.text_input).
     """
     buffer_key = f"buffer_{key}"
     main_value = st.session_state.get(key, "")
@@ -26,32 +28,30 @@ def safe_text_input(label, key, **kwargs):
     elif st.session_state.get(buffer_key) != main_value and key in st.session_state:
         st.session_state[buffer_key] = main_value
     
-    # Przechwytujemy on_change
+    # Przejmujemy on_change uzytkownika oraz jego args/kwargs
     user_on_change = kwargs.pop("on_change", None)
+    user_args = kwargs.pop("args", ())
+    user_kwargs = kwargs.pop("kwargs", {})
     
-    def _handle_change(*args, **kwargs):
-        # Aktualizujemy stan glowny z bufora
+    def _handle_change(*_args, **_kwargs):
+        """Synchronizuje buffer -> main, potem odpala akcje uzytkownika z jej argumentami."""
         st.session_state[key] = st.session_state[buffer_key]
-        # Odpalamy akcje uzytkownika (jesli sa)
         if user_on_change:
-            user_on_change()
+            user_on_change(*user_args, **user_kwargs)
     
-    # KLUCZOWA NAPRAWA: Wymuszamy value= zeby zapewnic synchronizacje
-    # nawet po wielokrotnym przelaczeniu slajdow
     return st.text_input(
         label,
         value=main_value,
         key=buffer_key,
         on_change=_handle_change,
-        **kwargs
+        **kwargs,
     )
+
 
 def safe_text_area(label, key, **kwargs):
     """
     Text area z buforem - nie przeladowuje na kazdy znak.
-    
-    NAPRAWA: Wymuszamy `value=` zeby Streamlit nie tracil wartosci 
-    po wielokrotnym odmontowaniu/zmontowaniu widgetu.
+    Wspiera on_change z args= i kwargs=.
     """
     buffer_key = f"buffer_{key}"
     main_value = st.session_state.get(key, "")
@@ -62,17 +62,18 @@ def safe_text_area(label, key, **kwargs):
         st.session_state[buffer_key] = main_value
     
     user_on_change = kwargs.pop("on_change", None)
+    user_args = kwargs.pop("args", ())
+    user_kwargs = kwargs.pop("kwargs", {})
     
-    def _handle_change(*args, **kwargs):
+    def _handle_change(*_args, **_kwargs):
         st.session_state[key] = st.session_state[buffer_key]
         if user_on_change:
-            user_on_change()
+            user_on_change(*user_args, **user_kwargs)
     
-    # KLUCZOWA NAPRAWA: Wymuszamy value= dla synchronizacji
     return st.text_area(
         label,
         value=main_value,
         key=buffer_key,
         on_change=_handle_change,
-        **kwargs
+        **kwargs,
     )
