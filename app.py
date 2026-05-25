f6b73c68dcb467193da4367f2944f69a9985409e
activezonepol <info@activezone.pl>
2026-05-25 23:11:45 +0200

    gradient na przerywnik atrakcje

--- app.py
+++ app.py
@@ -1529,8 +1529,8 @@ with col_form:
 
         st.file_uploader(
             "Zdjęcie tła (16:9):",
-            key="up_sek_0_img",
-            on_change=_make_upload_callback('sek_0_img')
+            key="up_sek_1_img",
+            on_change=_make_upload_callback('sek_1_img')
         )
             
     # -----------------------------------------------------------------------
