0237e8e1e2330bdbeb8a2b31b917f3b2d07f2d5e
activezonepol <info@activezone.pl>
2026-07-14 17:00:10 +0200

    Remove unused import from app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ import uuid
 from datetime import date, datetime
 import time
 import streamlit as st
-from db_utils import save_to_supabase, fetch_all_offers, _get_unique_project_name
+from db_utils import save_to_supabase, fetch_all_offers
 from supabase import create_client, Client
 from data_utils import _build_proj_dict
 from my_components import safe_text_input, safe_text_area, safe_checkbox, safe_selectbox, safe_number_input
