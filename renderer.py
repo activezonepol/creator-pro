1991a562bb5d3cdb3a8800e2948c5021fd3c9b02
activezonepol <info@activezone.pl>
2026-08-18 01:38:35 +0200

    Implement slide navigation HTML generation
    Add function to generate slide navigation HTML.

--- renderer.py
+++ renderer.py
@@ -3731,6 +3731,7 @@ def build_presentation(current_page="Strona Tytułowa", export_mode=False, activ
     components.html(_marker + full_html, height=800, scrolling=False)
     
     return ""
+    
     def get_slide_nav_html(acc='#FF6600'):
     """Wąski pionowy pasek nawigacji przy prawej krawędzi (w marginesie
     obok slajdu): kółka = strony, aktywna w kolorze akcentu, strzałki
