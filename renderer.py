3c27f5c9abaa8ab4ce06a218ceb775a2ebfa33a1
activezonepol <info@activezone.pl>
2026-08-19 14:42:29 +0200

    Add new regex patterns for video attributes

--- renderer.py
+++ renderer.py
@@ -828,6 +828,7 @@ _DYNAMIC_DATA_PATTERNS = [
     r'^h_hide_\d+$', r'^h_overline_\d+$', r'^h_title_\d+$', r'^h_subtitle_\d+$',
     r'^h_url_\d+$', r'^h_booking_\d+$', r'^h_amenities_\d+$', r'^h_text_\d+$',
     r'^h_advantages_\d+$', r'^hopt_label_\d+$',
+    r'^hvideo_url_\d+$', r'^hvideo_text_\d+$', r'^hvideo_start_\d+$', r'^hvideo_end_\d+$',
     r'^img_hotel_1_\d+$', r'^img_hotel_1b_\d+$', r'^img_hotel_2_\d+$', r'^img_hotel_3_\d+$',
     # --- Atrakcje ---
     r'^amain_\d+$', r'^asub_\d+$', r'^aday_\d+$', r'^atype_\d+$', r'^aopis_\d+$',
