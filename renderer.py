8620aef2ab420533cebc42332e6c795d3294f244
activezonepol <info@activezone.pl>
2026-09-08 17:16:46 +0200

    Update renderer.py

--- renderer.py
+++ renderer.py
@@ -2829,7 +2829,7 @@ def build_presentation(current_page="Strona Tytułowa", export_mode=False, activ
                 <div class="title-sub" style="margin-bottom:10px;">{str(get_data('l_sub','')).replace(chr(10),'<br>')}</div>
                 {h_d}
                 
-                <div class="metric-grid" style="grid-template-columns: 2.5fr 1fr; gap: 24px;">
+                <div class="metric-grid" style="grid-template-columns: 2.5fr 1fr; gap: 24px; margin-top:4px; padding-top:6px;">
                     <div><div class="metric-label">Trasa</div><div class="flight-val">{get_data('m_route','')}</div></div>
                     <div style="text-align:right;"><div class="metric-label">Limit bagażu</div><div class="flight-val">{get_data('m_luggage','')}</div></div>
                 </div>
