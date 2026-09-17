7a0a6d9ba9122f41deb8782f6ea30c8cebceceed
activezonepol <info@activezone.pl>
2026-09-17 13:37:09 +0200

    Add logo URL for Activezone in renderer.py

--- renderer.py
+++ renderer.py
@@ -461,6 +461,7 @@ defaults = {
                  'Badanie satysfakcji NPS — raport dla zarządu i działu HR\n'
                  'Wsparcie celów ESG, materiały cyfrowe zamiast druku'),
     'img_app_screen': 'https://ckmmtjuolhhboruujdni.supabase.co/storage/v1/object/public/nexa-images/default_user/LOGO/aplikacja1.jpg',
+    'logo_az': 'https://ckmmtjuolhhboruujdni.supabase.co/storage/v1/object/public/nexa-images/default_user/LOGO/Activezone%20trojkat_png%20przezroczyste.png',
     'brand_hide': False, 'brand_overline': 'IDENTYFIKACJA',
     'brand_title': 'MATERIAŁY\nBRANDINGOWE',
     'brand_subtitle': 'Komunikacja przed, w trakcie i po wyjeździe',
