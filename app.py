2eabb4c9b8ca490cd4c94f1e3c9297d37dd1fc94
activezonepol <info@activezone.pl>
2026-09-20 19:48:34 +0200

    Remove file uploader and render image slot

--- app.py
+++ app.py
@@ -3954,11 +3954,8 @@ with col_form:
         _ic2.color_picker("Kolor tytułu:", key="sek_2_txt", value=st.session_state.get("sek_2_txt", "#ffffff"))
         _ic3.color_picker("Kolor nadtytułu:", key="sek_2_sub_color", value=st.session_state.get("sek_2_sub_color", _sub_default))
 
-        st.file_uploader(
-            "Zdjęcie tła (16:9):",
-            key="up_sek_2_img",
-            on_change=_make_upload_callback('sek_2_img')
-        )
+        _country_gal = list_country_gallery(supabase, st.session_state.get('country_code', '') or 'XXX')
+        _render_img_slot(st.container(), "Zdjęcie tła (16:9):", "sek_2_img", _country_gal)
 
     # -----------------------------------------------------------------------
     # 18b. ESG (Odpowiedzialny partner)
